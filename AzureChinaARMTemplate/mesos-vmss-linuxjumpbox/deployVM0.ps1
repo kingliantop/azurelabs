bd99e222eb9b7e4ba59628161dd4a78327593960
StevenLian <kingliantop@gmail.com>
2016-11-11 21:40:33 +0800

    Azure China mesos deploymeny ready
    Updated for Azure China deployment

---
+++ AzureChinaARMTemplate/mesos-vmss-linuxjumpbox/deployVM0.ps1
@@ -0,0 +1,25 @@
+$VerbosePreference="Continue"
+$deployName="stevenmesosgp"
+$RGName=$deployName
+$locName="China North"
+#$locName="SouthEast Asia"
+#$locName="East US2"
+#$locName="West US"
+#$locName="Brazil South"
+#$locName="Central US"
+#$locName="East US"
+#$locName="SouthCentral US"
+#$locName="Japan East"
+#$locName="Japan West"
+#$locName="West Europe"
+#$locName="North Europe"
+#$locName="NorthCentral US"
+#$templateFile= "mesos-cluster-with-linux-jumpbox.json"
+#$templateFile= "mesos-cluster-with-windows-jumpbox.json"
+#$templateFile= "mesos-cluster-with-no-jumpbox.json"
+$templateFile= "mesos-cluster-with-linux-jumpbox.json"
+$templateParameterFile= "cluster.parameters.json"
+New-AzureRmResourceGroup -Name $RGName -Location $locName -Force
+
+echo New-AzureRmResourceGroupDeployment -Name $deployName -ResourceGroupName $RGName -TemplateFile $templateFile
+New-AzureRmResourceGroupDeployment -Name $deployName -ResourceGroupName $RGName -TemplateParameterFile $templateParameterFile -TemplateFile $templateFile
