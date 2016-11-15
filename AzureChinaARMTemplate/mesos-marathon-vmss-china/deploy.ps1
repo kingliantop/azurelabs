cc055b8e241e99788f12bcee4d1b6aae521b97e4
StevenLian <kingliantop@gmail.com>
2016-11-15 23:46:48 +0800

    Mesos deployment for china
    Mesos deployment for china

---
+++ AzureChinaARMTemplate/mesos-marathon-vmss-china/deploy.ps1
@@ -0,0 +1,11 @@
+$VerbosePreference="Continue"
+$deployName="stevenmesosnorgp"
+$RGName=$deployName
+$locName="China North"
+
+$templateFile= "mesos-cluster-vmss.json"
+$templateParameterFile= "cluster.parameters.json"
+New-AzureRmResourceGroup -Name $RGName -Location $locName -Force
+
+echo New-AzureRmResourceGroupDeployment -Name $deployName -ResourceGroupName $RGName -TemplateFile $templateFile
+New-AzureRmResourceGroupDeployment -Name $deployName -ResourceGroupName $RGName -TemplateParameterFile $templateParameterFile -TemplateFile $templateFile
